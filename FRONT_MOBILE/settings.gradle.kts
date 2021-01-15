f01dcb525a9781e58ab70ec506773763f97aedba
N4S <nassim.elabboubi@gmail.com>
2021-01-15 10:50:13 +0100

    FRONT_MOBILE | INIT | Initialisation du projet Kotlin Mobile SDK 27

---
+++ FRONT_MOBILE/settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+    
+}
+rootProject.name = "FRONT_MOBILE"
+
+
+include(":androidApp")
+include(":shared")
+
---
+++ FRONT_MOBILE/shared/src/commonMain/kotlin/me/nassim/shared/Platform.kt
@@ -0,0 +1,5 @@
+package me.nassim.shared
+
+expect class Platform() {
+    val platform: String
+}
