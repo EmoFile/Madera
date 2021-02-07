43e604d8296a720537263978c3df6e3659c4dd86
EmoFile <richard.sivera@viacesi.fr>
2021-02-07 12:03:37 +0100

    FRONT WEB | modify | création des devis amélioration

--- FRONT_WEB/js/main.js
+++ FRONT_WEB/js/main.js
@@ -1,99 +1,191 @@
-console.log("testA")
+let $gammes = [{"id_gamme": 1, "nom": "Portes"}, {"id_gamme": 2, "nom": "Murs"}]
+let $modules = [{"id_gamme": 1, "id_module": 1, "nom": "Porte en bois"},
+    {"id_gamme": 1, "id_module": 2, "nom": "Porte en PVC"},
+    {"id_gamme": 2, "id_module": 3, "nom": "Mur 4x2"},
+    {"id_gamme": 2, "id_module": 4, "nom": "Mur 5x2"}]
 $(() => {
-  let $navDiv = document.getElementById('navigation')
-  console.log($navDiv)
-  console.log("test")
-  let $ul = document.createElement('ul')
-  console.log($ul)
+    let $navDiv = document.getElementById('navigation')
+    console.log($navDiv)
+    console.log("test")
+    let $ul = document.createElement('ul')
+    console.log($ul)
 
-  let $li = document.createElement('li')
-  let $button = document.createElement('button')
-  $button.setAttribute('id',
-    `creation-devis`)
-  $ul.setAttribute('id', 'nav-bar')
-  $button.innerText = 'Creation devis'
+    let $li = document.createElement('li')
+    let $button = document.createElement('button')
+    $button.setAttribute('id',
+        `creation-devis`)
+    $ul.setAttribute('id', 'nav-bar')
+    $button.innerText = 'Creation devis'
 
-  $li.append($button)
-  $ul.append($li)
-  $navDiv.append($ul)
+    $li.append($button)
+    $ul.append($li)
+    $navDiv.append($ul)
 
-  $('#creation-devis').click(function () {
-    let devisJSON = {
-      "nom": $inputNomDevis.value,
-      "commercial": 1,
-      "client": 1,
-      "pieces": [],
-    }
-    let $mainDiv = document.getElementById('main-content')
-    let $devisForm = document.createElement('form')
-    $devisForm.setAttribute('id', 'devis-form')
+    $('#creation-devis').click(function () {
 
-    let $devisDiv = document.createElement('div')
-    $devisDiv.setAttribute('id', 'devis-div')
+        let $mainDiv = document.getElementById('main-content')
+        $mainDiv.setAttribute('class', 'container-fluid')
+        let $devisForm = document.createElement('form')
+        $devisForm.setAttribute('id', 'devis-form')
+        $devisForm.setAttribute('class', 'container-fluid')
 
-    //region top-devis
-    let $topDevisDiv = document.createElement('div')
-    $topDevisDiv.setAttribute('id', 'top-devis-div')
-    let $labelNomDevis = document.createElement('label')
-    $labelNomDevis.setAttribute('for', 'nom')
-    $labelNomDevis.innerText = "Nom du devis : "
-    let $inputNomDevis = document.createElement('input')
-    $inputNomDevis.setAttribute('id', 'nom')
-    $inputNomDevis.setAttribute('name', 'nom')
-    $inputNomDevis.setAttribute('type', 'text')
+        let $devisDiv = document.createElement('div')
+        $devisDiv.setAttribute('id', 'devis-div')
+        $devisDiv.setAttribute('class', 'container-fluid')
 
-    $topDevisDiv.append($labelNomDevis)
-    $topDevisDiv.append($inputNomDevis)
-    //endregion
+        //region top-devis
+        let $topDevisDiv = document.createElement('div')
+        $topDevisDiv.setAttribute('id', 'top-devis-div')
+        $topDevisDiv.setAttribute('class', 'row')
+        let $labelNomDevis = document.createElement('label')
+        $labelNomDevis.setAttribute('for', 'nom')
+        $labelNomDevis.innerText = "Nom du devis : "
+        let $inputNomDevis = document.createElement('input')
+        $inputNomDevis.setAttribute('id', 'nom')
+        $inputNomDevis.setAttribute('name', 'nom')
+        $inputNomDevis.setAttribute('type', 'text')
 
-    //region center-devis
-    let $centerDevisDiv = document.createElement('div')
-    let $centerLeftDevisDiv = document.createElement('div')
-    let $centerRightDevisDiv = document.createElement('div')
-    $centerLeftDevisDiv.setAttribute('id', 'center-left-devis-div')
-    $centerRightDevisDiv.setAttribute('id', 'center-right-devis-div')
-    $centerDevisDiv.setAttribute('id', 'center-devis-div')
-    let $pAjtPiece = document.createElement('p')
-    $pAjtPiece.innerText = 'Ajouter une pièce'
-    let $buttonAjtPiece = document.createElement('button')
-    $buttonAjtPiece.setAttribute('type',
-      `button`)
-    $buttonAjtPiece.setAttribute('id',
-      `ajouter-piece`)
-    $buttonAjtPiece.innerText = "+"
-    $centerLeftDevisDiv.append($pAjtPiece)
-    $centerLeftDevisDiv.append($buttonAjtPiece)
-    $centerDevisDiv.append($centerLeftDevisDiv)
-    $centerDevisDiv.append($centerRightDevisDiv)
-    //endregion
+        $topDevisDiv.append($labelNomDevis)
+        $topDevisDiv.append($inputNomDevis)
+        //endregion
 
-    //region bottom-devis
-    let $bottomDevisDiv = document.createElement('div')
-    $bottomDevisDiv.setAttribute('id', 'bottom-devis-div')
-    let $pPriceDevis = document.createElement('p')
-    $pPriceDevis.setAttribute('id', 'price-devis')
-    $pPriceDevis.innerText = "0"
-    let $pTextPriceDevis = document.createElement('p')
-    $pTextPriceDevis.innerText = "Prix :"
-    $bottomDevisDiv.append($pTextPriceDevis)
-    $bottomDevisDiv.append($pPriceDevis)
-    //endregion
+        //region center-devis
+        let $centerDevisDiv = document.createElement('div')
+        $centerDevisDiv.setAttribute('class', 'row')
+        let $centerLeftDevisDiv = document.createElement('div')
+        let $centerRightDevisDiv = document.createElement('div')
+        $centerLeftDevisDiv.setAttribute('id', 'center-left-devis-div')
+        $centerLeftDevisDiv.setAttribute('class', 'col border')
+        $centerRightDevisDiv.setAttribute('id', 'center-right-devis-div')
+        $centerRightDevisDiv.setAttribute('class', 'col border')
+        $centerDevisDiv.setAttribute('id', 'center-devis-div')
+        $centerDevisDiv.setAttribute('class', 'row')
+
+        let $buttonAjtPiece = document.createElement('button')
+        $buttonAjtPiece.setAttribute('type', `button`)
+        $buttonAjtPiece.setAttribute('id', `ajouter-piece`)
+        $buttonAjtPiece.innerText = "Ajouter une pièce"
+        $centerLeftDevisDiv.append($buttonAjtPiece)
+        $centerDevisDiv.append($centerLeftDevisDiv)
+        $centerDevisDiv.append($centerRightDevisDiv)
+        //endregion
+
+        //region bottom-devis
+        let $bottomDevisDiv = document.createElement('div')
+        $bottomDevisDiv.setAttribute('id', 'bottom-devis-div')
+        $bottomDevisDiv.setAttribute('class', 'row')
+        let $pPriceDevis = document.createElement('p')
+        $pPriceDevis.setAttribute('id', 'price-devis')
+        $pPriceDevis.innerText = "0"
+        let $pTextPriceDevis = document.createElement('p')
+        $pTextPriceDevis.innerText = "Prix :"
+        $bottomDevisDiv.append($pTextPriceDevis)
+        $bottomDevisDiv.append($pPriceDevis)
+        //endregion
 
 
-    $devisDiv.append($topDevisDiv)
-    $devisDiv.append($centerDevisDiv)
-    $devisDiv.append($bottomDevisDiv)
-    $devisForm.append($devisDiv)
-    $mainDiv.append($devisForm)
+        $devisDiv.append($topDevisDiv)
+        $devisDiv.append($centerDevisDiv)
+        $devisDiv.append($bottomDevisDiv)
+        $devisForm.append($devisDiv)
+        $mainDiv.append($devisForm)
 
-    $('#ajouter-piece').click(function () {
-      let devisJSON = {
-        "nom": $inputNomDevis.value,
-        "commercial": 1,
-        "client": 1,
-        "pieces": [],
-      }
-      console.log(devisJSON)
+        $('#ajouter-piece').click(function () {
+
+            let $pieceDiv = document.createElement('div')
+            $pieceDiv.setAttribute('id', 'piece-div')
+            $pieceDiv.setAttribute('class', 'container-fluid border-1')
+            let $topPieceDiv = document.createElement('div')
+            $topPieceDiv.setAttribute('class', 'row')
+            $topPieceDiv.setAttribute('id', 'top-piece-div')
+
+            let $centerPieceDiv = document.createElement('div')
+            $centerPieceDiv.setAttribute('class', 'container-fluid')
+            $centerPieceDiv.setAttribute('id', 'center-piece-div')
+
+            let $bottomPieceDiv = document.createElement('div')
+            $bottomPieceDiv.setAttribute('class', 'row')
+            $bottomPieceDiv.setAttribute('id', 'bottom-piece-div')
+
+            let $pieceName = document.createElement('input')
+            $pieceName.setAttribute('id', 'piece-name')
+            $pieceName.setAttribute('type', 'text')
+            $pieceName.setAttribute('name', 'piece-name')
+            let $pieceNameLabel = document.createElement('label')
+            $pieceNameLabel.setAttribute('for', 'piece-name')
+            $pieceNameLabel.innerText = "Nom de la pièce"
+            $topPieceDiv.append($pieceNameLabel)
+            $topPieceDiv.append($pieceName)
+            $pieceDiv.append($topPieceDiv)
+
+            let $buttonValiderPiece = document.createElement('button')
+            $buttonValiderPiece.setAttribute('type', `button`)
+            $buttonValiderPiece.setAttribute('id', `valider-piece`)
+            $buttonValiderPiece.innerText = "Valider"
+
+            let $buttonAjouterModule = document.createElement('button')
+            $buttonAjouterModule.setAttribute('type', `button`)
+            $buttonAjouterModule.setAttribute('id', `ajouter-module`)
+            $buttonAjouterModule.innerText = "Ajouter un module"
+
+            $topPieceDiv.append($buttonAjouterModule)
+            $bottomPieceDiv.append($buttonValiderPiece)
+            $pieceDiv.append($centerPieceDiv)
+            $pieceDiv.append($bottomPieceDiv)
+            $centerRightDevisDiv.append($pieceDiv)
+
+            $('#ajouter-module').click(function () {
+                let $selectModuleDiv = document.createElement('div')
+                $selectModuleDiv.setAttribute('class',
+                    'row')
+                let $selectModule = document.createElement('select')
+                $selectModule.setAttribute('id',
+                    'selec-module-' + (document.getElementById('center-piece-div').childElementCount + 1))
+
+                for (let i = 0; i < $gammes.length; i++) {
+                    let $optGroup = document.createElement('optgroup')
+                    $optGroup.setAttribute('label', $gammes[i].nom)
+                    for (let j = 0; j < $modules.length; j++) {
+                        if ($modules[j].id_gamme === $gammes[i].id_gamme) {
+                            let $option = document.createElement('option')
+                            $option.setAttribute('value', $modules[j].id_module)
+                            $option.innerText = $modules[j].nom
+                            $optGroup.append($option)
+                        }
+                    }
+                    $selectModule.append($optGroup)
+                    $selectModuleDiv.append($selectModule)
+                    $centerPieceDiv.append($selectModuleDiv)
+                }
+            });
+            $('#valider-piece').click(function () {
+                let devisJSON = {
+                    "nom": document.getElementById('nom').value,
+                    "commercial": 1,
+                    "client": 1,
+                    "pieces": [],
+                }
+                console.log(devisJSON['pieces'].length)
+                console.log(devisJSON['pieces'].length + 1)
+                let $recapPieceDiv = document.createElement('div')
+                $recapPieceDiv.setAttribute('id', `recap-piece-` + (devisJSON['pieces'].length + 1))
+                $recapPieceDiv.setAttribute('class', `row`)
+                let $recapPieceNom = document.createElement('p')
+                $recapPieceNom.setAttribute('class', `col`)
+                $recapPieceNom.innerText = document.getElementById('piece-name').value
+                let $deleteBtnRecapPiece = document.createElement('button')
+                $deleteBtnRecapPiece.setAttribute('class', `col btn btn-danger`)
+                $deleteBtnRecapPiece.setAttribute('type', `button`)
+                $deleteBtnRecapPiece.innerText = 'X'
+                $recapPieceDiv.append($recapPieceNom)
+                $recapPieceDiv.append($deleteBtnRecapPiece)
+                $centerLeftDevisDiv.append($recapPieceDiv)
+                devisJSON["pieces"].push({
+                    "nom": document.getElementById('piece-name').value,
+                    "modules": []
+                })
+                console.log(devisJSON)
+            });
+        });
     });
-  });
 });
